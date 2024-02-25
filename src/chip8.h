013193b33819646a46d3ea5b63d621f8ea22faee
chunou <johnkaushagen@gmail.com>
2024-02-25 11:57:09 -0500

    add inital stuff for screen

--- src/chip8.h
+++ src/chip8.h
@@ -8,8 +8,12 @@
 #define FONTSET_SIZE 80
 #define FONTSET_OFFSET 0x50
 
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+
 struct Chip8 {
     uint8_t memory[RAM_SIZE];
+    uint8_t screen[SCREEN_WIDTH][SCREEN_HEIGHT];
 };
 
 void init_chip8(struct Chip8 *chip8);
