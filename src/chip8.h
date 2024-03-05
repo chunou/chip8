ecb6d4a0f36e26cf5efb4520a31365fef287f3b0
chunou <johnkaushagen@gmail.com>
2024-03-05 11:33:13 -0500

    add stack

--- src/chip8.h
+++ src/chip8.h
@@ -6,6 +6,8 @@
 #define RAM_SIZE 4096
 #define PROGRAM_START 0x200
 
+#define STACK_SIZE 16
+
 #define FONTSET_SIZE 80
 #define FONTSET_OFFSET 0x50
 
@@ -15,6 +17,8 @@
 typedef struct {
     uint8_t memory[RAM_SIZE];
     uint8_t screen[SCREEN_WIDTH][SCREEN_HEIGHT];
+    uint16_t stack[STACK_SIZE];
+
 } Chip8;
 
 void init_chip8(Chip8 *chip8);
