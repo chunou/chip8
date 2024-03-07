7c296ca4e4065b3e39f805db93a742158b07f335
chunou <johnkaushagen@gmail.com>
2024-03-07 11:56:48 -0500

    stuf

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 
@@ -14,10 +15,11 @@ int main(int argc, char *argv[]) {
     init_chip8(&chip8);
     chip8.delay_timer = 60;
     chip8.sound_timer = 30;
-    chip8.memory[0] = 0x4F;
-    chip8.memory[1] = 0xA3;
+    chip8.memory[0] = 0xD0;
+    chip8.memory[1] = 0x05;
     chip8.index_register = 0;
-    chip8.I = FONTSET_OFFSET+55;
+    chip8.I = 0;
+    chip8.I = FONTSET_OFFSET+35;
 
     Display display;
     init_display(&display);;
@@ -35,13 +37,40 @@ int main(int argc, char *argv[]) {
         SDL_RenderClear(display.renderer);
 
         // Do chip8 stuff here
+        uint16_t nibble_mask = 0b1111;
+        uint16_t instruction = fetch_instruction(&chip8);
+        uint8_t nibble0 = (instruction & (nibble_mask << 12)) >> 12;
+        uint8_t nibble1 = (instruction & (nibble_mask << 8)) >> 8;
+        uint8_t nibble2 = (instruction & (nibble_mask << 4)) >> 4;
+        uint8_t nibble3 = (instruction & (nibble_mask));
+        printf("n0: %X, n1: %X, n2: %X, n3: %X\n", nibble0, nibble1, nibble2, nibble3);
+
+        // Decode
+        switch (nibble0) {
+        case 0x0:
+            if (instruction == 0x00E0) {
+                clear_screen(&chip8);
+            }
+            break;
         
+        case 0xD:
+            draw_to_display(&chip8, nibble1, nibble2, nibble3);
+            break;
+        
+        default:
+            printf("Fallthrough on instruction %04X\n", instruction);
+            break;
+        }
+            // Fetch instruction
+            // Decode instruction
+            // Execute instruction
 
         // Compose Scene
         SDL_SetRenderDrawColor(display.renderer, 255, 255, 255, 0);
         compose_scene(&display, &chip8.screen[0][0], SCREEN_WIDTH, SCREEN_HEIGHT);
 
         // Present Renderer
+        // Mabye we only do this if the screen has changed?
         SDL_RenderPresent(display.renderer);
     }
 
