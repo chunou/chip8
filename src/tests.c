2a455905828b8e878e08d146fab80a8d3478ff3d
chunou <johnkaushagen@gmail.com>
2024-03-15 13:58:02 -0400

    flush buffer before assert so printing happens

--- src/tests.c
+++ src/tests.c
@@ -9,12 +9,14 @@ void test_init_chip8() {
     Chip8 chip8;
     init_chip8(&chip8);
     printf("test_init_chip8:\n");
-    printf("\tchecking ram is cleared... ");
+    printf("  checking ram is cleared... ");
+    fflush(stdout);
     for (size_t i=0; i<RAM_SIZE; ++i) {
         assert(chip8.ram[i] == 0);
     }
     printf("ok\n");
 }
+
 int main(int argc, char *argv[]) {
     test_init_chip8();
     printf("All tests passed!\n");
